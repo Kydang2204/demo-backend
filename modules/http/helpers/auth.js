7545b3bdd10378178be21400b07eb5ab1b8428f5
KY <83952965+Kydang2204@users.noreply.github.com>
2022-12-23 21:04:53 +0700

    Sign in api (#3)
    * add dotenv
    
    * sign in api

--- modules/http/helpers/auth.js
+++ modules/http/helpers/auth.js
@@ -1,6 +1,6 @@
 const knex = require('../../../database')
 
-module.exports.checkEmail = async (email) => {
+function _checkEmailFormat (email) {
   const partern = /\S+@\S+\.\S+/
   const valid = partern.test(email)
   if (!valid) {
@@ -9,7 +9,16 @@ module.exports.checkEmail = async (email) => {
       reason: 'Email format is wrong'
     }
   }
+  return { valid: true }
+}
 
+module.exports.checkEmailFormat = _checkEmailFormat
+
+module.exports.checkEmail = async (email) => {
+  const result = _checkEmailFormat(email)
+  if (!result.valid) {
+    return result
+  }
   const user = await knex('Users').where('email', email).first()
   if (user) {
     return {
@@ -20,8 +29,8 @@ module.exports.checkEmail = async (email) => {
   return { valid: true }
 }
 
-module.exports.checkPassword = (pw) => {
-  if (!pw || (pw.length <= 8 && pw.length >= 20)) {
+module.exports.checkPasswordLength = (pw) => {
+  if (!pw || pw.length <= 8 || pw.length >= 20) {
     return {
       valid: false,
       reason: 'Password must be between 8-20 characters'
--- modules/http/routes/auth.api.js
+++ modules/http/routes/auth.api.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const knex = require('../../../database')
+const jwt = require('jsonwebtoken')
+const cookie = require('cookie')
+const expressjwt = require('express-jwt')
 const auth = require('../helpers/auth.js')
 const router = express.Router()
 
+const cookieOpts = {
+  httpOnly: true,
+  secure: false,
+  domain: 'localhost',
+  path: '/',
+  sameSite: false
+}
+
 router.post('/sign-up', async (req, res) => {
   const { email, password, firstName, lastName } = req.body
-  let result = auth.checkPassword(password)
+  let result = auth.checkPasswordLength(password)
   if (!result.valid) {
     return res.status(400).json(result)
   }
@@ -19,7 +30,7 @@ router.post('/sign-up', async (req, res) => {
   const createdAt = new Date().toISOString().replace('Z', '').replace('T', ' ')
   try {
     await knex('Users').insert({
-      email,
+      email: email.toLowerCase(),
       password: await bcrypt.hash(password, 10),
       firstName,
       lastName,
@@ -39,6 +50,54 @@ router.post('/sign-up', async (req, res) => {
     console.log(err)
     return res.status(500).json({ message: 'An internal error is occurring' })
   }
+})
+
+router.post('/sign-in', async (req, res) => {
+  const { email, password } = req.body
+
+  let result = auth.checkEmailFormat(email)
+  if (!result.valid) {
+    return res.status(400).json(result)
+  }
+
+  result = auth.checkPasswordLength(password)
+
+  if (!result.valid) {
+    return res.status(400).json(result)
+  }
+
+  const user = await knex('Users').where('email', email.toLowerCase()).first()
+  result = await bcrypt.compare(password, user.password)
+  if (!result) {
+    return res.status(400).json({
+      valid: false,
+      reason: 'Password is wrong'
+    })
+  }
+
+  const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  const refreshToken = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' })
+
+  const createdAt = new Date().toISOString().replace('Z', '').replace('T', ' ')
+  await knex('Tokens').insert({
+    userId: user.id,
+    refreshToken,
+    expiresIn: new Date(Date.now() + 30 * 864e5).toISOString(),
+    createdAt,
+    updatedAt: createdAt
+  })
+
+  res.cookie('accessToken', accessToken, cookieOpts)
+  return res.status(200).json({
+    user: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email,
+      displayName: user.firstName + ' ' + user.lastName
+    },
+    accessToken,
+    refreshToken
+  })
 })
 
 module.exports = router
