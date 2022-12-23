31b4e9a23a240e8bdd7462ccf532ff2c643ca22b
andychicken <83952965+Kydang2204@users.noreply.github.com>
2022-12-23 14:45:27 +0700

    refactor

--- modules/http/routes/auth.api.js
+++ modules/http/routes/auth.api.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const router = express.Router()
 const knex = require('../../../database')
+const auth = require('../helpers/auth.js')
+const router = express.Router()
 
 router.post('/sign-up', async (req, res) => {
   const { email, password, firstName, lastName } = req.body
-  let result = _checkPassword(password)
+  let result = auth.checkPassword(password)
   if (!result.valid) {
     return res.status(400).json(result)
   }
 
-  result = await _checkEmail(email)
+  result = await auth.checkEmail(email)
   if (!result.valid) {
     return res.status(400).json(result)
   }
@@ -39,35 +40,5 @@ router.post('/sign-up', async (req, res) => {
     return res.status(500).json({ message: 'An internal error is occurring' })
   }
 })
-
-async function _checkEmail (email) {
-  const partern = /\S+@\S+\.\S+/
-  const valid = partern.test(email)
-  if (!valid) {
-    return {
-      valid: false,
-      reason: 'Email format is wrong'
-    }
-  }
-
-  const user = await knex('Users').where('email', email).first()
-  if (user) {
-    return {
-      valid: false,
-      reason: 'Email is dupplicated'
-    }
-  }
-  return { valid: true }
-}
-
-function _checkPassword (pw) {
-  if (!pw || (pw.length <= 8 && pw.length >= 20)) {
-    return {
-      valid: false,
-      reason: 'Password must be between 8-20 characters'
-    }
-  }
-  return { valid: true }
-}
 
 module.exports = router
